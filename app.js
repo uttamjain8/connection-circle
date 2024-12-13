c906608cfc879740417dd374a939924e39309637
uttamjain8 <54641120+uttamjain8@users.noreply.github.com>
2024-12-13 19:25:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function render() {
         })
         .attr("alignment-baseline", "middle")
         .text(d => d.id)
-        .style("font-size", "18px")
+        .style("font-size", "25px")
         .style("fill", d => entityColors[d.id]);
 
     // Draw directed connections with adjusted paths and colors
